872a3a0768c007d8ea6b3bbd64c52a6f1bd97780
Saeed Falsafin <sabloger@gmail.com>
2017-11-26 18:16:25 +0330

    multi thereading on send collections

--- telectapi/management/commands/send_sys_sender.py
+++ telectapi/management/commands/send_sys_sender.py
@@ -1,3 +1,4 @@
+import threading
 from datetime import datetime
 from time import sleep
 
@@ -33,77 +34,87 @@ class Command(BaseCommand):
                 continue
 
             for collection in user.collection_set.all():
-                print("collection:", collection)
+                # self.send_collection(collection, telegram_api, user_client, sys_sender_client)
+                sleep(1)
+                t = threading.Thread(target=self.send_collection,
+                                     args=(collection, telegram_api, user_client, sys_sender_client))
+                t.start()
+
+        self.stdout.write("End of work!")
+
+    def send_collection(self, collection, telegram_api, user_client, sys_sender_client):
+        print("collection:", collection)
+
+        try:
+            sources = collection.source_set.all()
+
+            sleep(1)
+            destination_chan = telegram_api.get_dest_channel(user_client, sys_sender_client,
+                                                             collection.destination_data['id'],
+                                                             sources)
+        except:
+            return
+
+        for source in sources:
+            try:
+                try:
+                    src = Source.objects.get(id=source.id, last_fm_time__gt=datetime.now().timestamp() - 30,
+                                             are_collecting=True)
+                    if src.are_collecting:
+                        continue
+                    else:
+                        source.are_collecting = True
+                        source.save()
+                except:
+                    source.are_collecting = True
+                    source.save()
+
+                print("collection:", collection, "source:", source)
+
+                sleep(3)
 
                 try:
-                    sources = collection.source_set.all()
+                    channel = telegram_api.get_channel(sys_sender_client, source.source_data['id'])
 
-                    sleep(1)
-                    destination_chan = telegram_api.get_dest_channel(user_client, sys_sender_client,
-                                                                     collection.destination_data['id'],
-                                                                     sources)
-                except:
-                    continue
+                except ValueError:
+                    sys_sender_client(ResolveUsernameRequest(source.source_data['username']))
+                    channel = telegram_api.get_channel(sys_sender_client, source.source_data['id'])
 
-                for source in sources:
+                try:
+                    total, messages, senders = user_client.get_message_history(channel, limit=20)
+
+                except ChannelInvalidError:
                     try:
-                        try:
-                            src = Source.objects.get(id=source.id, last_fm_time__gt=datetime.now().timestamp() - 30,
-                                                     are_collecting=True)
-                            if src.are_collecting:
-                                continue
-                            else:
-                                source.are_collecting = True
-                                source.save()
-                        except:
-                            source.are_collecting = True
-                            source.save()
+                        print("ChannelInvalidError:", source.source_data['username'])
+                        user_client(ResolveUsernameRequest(source.source_data['username']))
+                        total, messages, senders = user_client.get_message_history(channel, limit=20)
 
-                        print("source:", source)
-
-                        try:
-                            channel = telegram_api.get_channel(sys_sender_client, source.source_data['id'])
-
-                        except ValueError:
-                            sys_sender_client(ResolveUsernameRequest(source.source_data['username']))
-                            channel = telegram_api.get_channel(sys_sender_client, source.source_data['id'])
-
-                        sleep(1)
-                        try:
-                            total, messages, senders = user_client.get_message_history(channel, limit=20)
-
-                        except ChannelInvalidError:
-                            try:
-                                print("ChannelInvalidError:", source.source_data['username'])
-                                user_client(ResolveUsernameRequest(source.source_data['username']))
-                                total, messages, senders = user_client.get_message_history(channel, limit=20)
-
-                            except:
-                                source.are_collecting = False
-                                source.save()
-                                continue
-
-                        messages = messages[::-1]  # reversing list to be asc (because telegram desc mid)
-
-                        for msg in messages:
-                            if type(msg) is Message:
-                                if msg.date.timestamp() > source.last_fm_time.timestamp():
-                                    print("msg:", msg.id)
-
-                                    sys_sender_client(ForwardMessagesRequest(
-                                        from_peer=channel,
-                                        id=[msg.id],
-                                        to_peer=destination_chan,
-                                        silent=True
-                                    ))
-
-                                    source.last_fm_time = msg.date
-                                    source.last_fm_id = msg.id
-                                    source.save()
-
-                        source.are_collecting = False
-                        source.save()
                     except:
                         source.are_collecting = False
                         source.save()
-        self.stdout.write("End of work!")
+                        continue
+
+                messages = messages[::-1]  # reversing list to be asc (because telegram desc mid)
+
+                for msg in messages:
+                    if type(msg) is Message:
+                        if msg.date.timestamp() > source.last_fm_time.timestamp():
+                            print("msg:", msg.id)
+
+                            sleep(3)
+                            sys_sender_client(ForwardMessagesRequest(
+                                from_peer=channel,
+                                id=[msg.id],
+                                to_peer=destination_chan,
+                                silent=True
+                            ))
+
+                            source.last_fm_time = msg.date
+                            source.last_fm_id = msg.id
+                            source.save()
+
+                source.are_collecting = False
+                source.save()
+            except:
+                source.are_collecting = False
+                source.save()
